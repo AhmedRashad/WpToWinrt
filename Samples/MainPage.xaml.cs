0cd6ea791d3ab85ad7d88c7f297be4a6bd9c5586
ahmedrashad <ahmedrashad.1990@hotmail.com>
2015-10-24 20:16:36 +0200

    -Ported Expander View Sample.

--- Samples/MainPage.xaml.cs
+++ Samples/MainPage.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using WpToWinrt.Common;
+using WpToWinrt.Controls;
 
 
 namespace Samples
@@ -48,6 +38,7 @@ namespace Samples
             _samples = new SampleItem[]
             {
             new SampleItem(typeof(RatingControlSample),"Rating Control","simple control for star-based rating"),
+            new SampleItem(typeof(ExpanderViewSample),"Expander View","shows sub-items similar to the new email app"),
 
             };
         }
---
+++ Samples/Samples/ExpanderViewSample.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using WpToWinrt.Common;
+
+
+namespace Samples
+{
+
+    public sealed partial class ExpanderViewSample : Page
+    {
+        private readonly NavigationHelper _navigationHelper;
+        public ExpanderViewSample()
+        {
+            this.InitializeComponent();
+            _navigationHelper = new NavigationHelper(this);
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            _navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            _navigationHelper.OnNavigatedFrom(e);
+        }
+    }
+}
